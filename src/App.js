f0f887c2a0fc44b2e5e2d517fd90856cc4191398
H_A _Opu <wwwhasib4@gmail.com>
2022-04-04 06:50:01 +0600

    setup route to all components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import Reviews from "./Components/Reviews/Reviews";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Blogs from "./Components/Blogs/Blogs";
+import About from "./Components/About/About";
+import NotFound from "./Components/NotFound/NotFound";
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
