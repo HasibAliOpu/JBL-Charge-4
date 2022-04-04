01eaf297f4a9b9043d56c213a6c6a59d3f5de890
H_A _Opu <wwwhasib4@gmail.com>
2022-04-04 07:09:36 +0600

    Navigation bar added using CustomLink

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import CustomLink from "./../CustomLink/CustomLink";
 const Header = () => {
   return (
-    <div>
-      <h1 className="text-6xl">Header Components</h1>
+    <div className="flex justify-center gap-10 my-5 font-medium text-xl text-orange-400">
+      <CustomLink to="/">Home</CustomLink>
+      <CustomLink to="/reviews">Reviews</CustomLink>
+      <CustomLink to="/dashboard">Dashboard</CustomLink>
+      <CustomLink to="/blogs">Blogs</CustomLink>
+      <CustomLink to="/about">About</CustomLink>
     </div>
   );
 };
