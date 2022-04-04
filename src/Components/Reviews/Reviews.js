da72d4c12ccb8156278aeac0ebe9e009e84b21a3
H_A _Opu <wwwhasib4@gmail.com>
2022-04-04 09:25:59 +0600

    reviews section done

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,9 +1,21 @@
 import React from "react";
+import LoadReviews from "../Hooks/LoadReviews";
+import Review from "../Review/Review";
 
 const Reviews = () => {
+  const [reviews, setReviews] = LoadReviews();
   return (
     <div>
-      <h1 className="text-6xl">Review Components</h1>
+      <div className="mb-5">
+        <h1 className="text-center text-4xl font-medium">
+          Customer Reviews ({reviews.length})
+        </h1>
+        <div className="grid md:grid-cols-3 gap-10 ml-7 md:p-10">
+          {reviews.map((review) => (
+            <Review key={review.id} review={review}></Review>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
