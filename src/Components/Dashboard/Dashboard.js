f607547abf109742db1176834a156e6bd7722760
H_A _Opu <wwwhasib4@gmail.com>
2022-04-05 14:18:17 +0600

    making responsive chart

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -6,6 +6,7 @@ import {
   BarChart,
   CartesianGrid,
   Legend,
+  ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis,
@@ -17,92 +18,94 @@ const Dashboard = () => {
       month: "Mar",
       investment: 100000,
       sell: 241,
-      revenue: 10401,
+      revenue: 53401,
     },
     {
       month: "Apr",
       investment: 200000,
       sell: 4220,
-      revenue: 24500,
+      revenue: 64500,
     },
     {
       month: "May",
       investment: 500000,
       sell: 7260,
-      revenue: 67010,
+      revenue: 199010,
     },
     {
       month: "Jun",
       investment: 500000,
       sell: 5200,
-      revenue: 40405,
+      revenue: 404050,
     },
     {
       month: "Jul",
       investment: 600000,
       sell: 6000,
-      revenue: 50900,
+      revenue: 150900,
     },
     {
       month: "Aug",
       investment: 700000,
       sell: 6440,
-      revenue: 61000,
+      revenue: 361000,
     },
   ];
   return (
-    <div>
+    <div className="p-10 my-5">
       <div className="my-10 flex justify-center">
-        <div>
+        <div className="w-full h-80">
           <h1 className="text-center text-3xl font-mono font-bold text-violet-700">
             MONTH WISE SELL
           </h1>
-          <AreaChart
-            width={730}
-            height={350}
-            data={data}
-            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-          >
-            <defs>
-              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-              </linearGradient>
-              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-              </linearGradient>
-            </defs>
-            <XAxis dataKey="month" />
-            <YAxis />
-            <CartesianGrid strokeDasharray="3 3" />
-            <Tooltip />
-            <Area
-              type="monotone"
-              dataKey="sell"
-              stroke="green"
-              fillOpacity={1}
-              fill="url(#colorPv)"
-            />
-            <Legend verticalAlign="top" height={36} />
-          </AreaChart>
+          <ResponsiveContainer>
+            <AreaChart
+              data={data}
+              margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+            >
+              <defs>
+                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                  <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                </linearGradient>
+                <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                  <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                </linearGradient>
+              </defs>
+              <XAxis dataKey="month" />
+              <YAxis />
+              <CartesianGrid strokeDasharray="3 3" />
+              <Tooltip />
+              <Area
+                type="monotone"
+                dataKey="sell"
+                stroke="green"
+                fillOpacity={1}
+                fill="url(#colorPv)"
+              />
+              <Legend verticalAlign="top" height={36} />
+            </AreaChart>
+          </ResponsiveContainer>
         </div>
       </div>
       <div className="my-10 flex justify-center w-full">
-        <div>
+        <div className="w-full h-80">
           <h1 className="text-center text-3xl font-mono font-bold mb-5">
             <span className="text-indigo-500">INVESTMENT</span> VS
             <span className="text-green-500"> REVENUE</span>
           </h1>
-          <BarChart width={730} height={350} data={data}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="month" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="investment" fill="#8884d8" />
-            <Bar dataKey="revenue" fill="#82ca9d" />
-          </BarChart>
+          <ResponsiveContainer>
+            <BarChart data={data}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="month" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="investment" fill="#8884d8" />
+              <Bar dataKey="revenue" fill="#82ca9d" />
+            </BarChart>
+          </ResponsiveContainer>
         </div>
       </div>
     </div>
